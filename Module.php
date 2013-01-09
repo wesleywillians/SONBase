bfea0f315cc747716a73e0ce061b6e7e157390b5
Wesley Willians <wesleywillians@gmail.com>
2013-01-09 09:28:52 -0200

    Adicionando Module.php dentro de SRC

--- Module.php
+++ Module.php
@@ -1,32 +1,7 @@
 <?php
 /**
- * Zend Framework (http://framework.zend.com/)
- *
- * @link      http://github.com/zendframework/ZendSkeletonApplication for the canonical source repository
- * @copyright Copyright (c) 2005-2012 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
+ * This file is placed here for compatibility with Zendframework 2's ModuleManager.
+ * It allows usage of this module even without composer.
+ * The original Module.php is in 'src/SONBase' in order to respect PSR-0
  */
-
-namespace SONBase;
-
-use Zend\Mvc\MvcEvent;
-
-class Module
-{
-    
-    public function getConfig()
-    {
-        return include __DIR__ . '/config/module.config.php';
-    }
-
-    public function getAutoloaderConfig()
-    {
-        return array(
-            'Zend\Loader\StandardAutoloader' => array(
-                'namespaces' => array(
-                    __NAMESPACE__ => __DIR__ . '/src/' . __NAMESPACE__,
-                ),
-            ),
-        );
-    }
-}
+require_once __DIR__ . '/src/SONBase/Module.php';
